ae5f0c86bf8b1b5dc68053376007ce4c26ba190c
LeoZandvliet <leozandvliet@gmail.com>
2025-06-14 01:35:49 +0200

    Update example-scripts.js

--- example-includes/js/example-scripts.js
+++ example-includes/js/example-scripts.js
@@ -2,10 +2,20 @@ function Compare() {
     var serializeResults = $("form[name='testForm']").serialize();
     var serializeAllResults = $("form[name='testForm']").serializeAll();
 
-    $("#results").html(
-        "<p><strong>.serialize() output...</strong></p>" +
+    $("#results").append(
+        "<p><strong>$('form').serialize() output...</strong></p>" +
         "<p class=\"red\">" + serializeResults + "</p>" +
-        "<p><strong>.serializeAll() output...</strong></p>" +
+        "<p><strong>$('form').serializeAll() output...</strong></p>" +
+        "<p class=\"green\">" + serializeAllResults + "</p>"
+    );
+
+    serializeResults = $("input, select").serialize();
+    serializeAllResults = $("input, select").serializeAll();
+
+    $("#results").append(
+        "<p><strong>$('input, select').serialize() output...</strong></p>" +
+        "<p class=\"red\">" + serializeResults + "</p>" +
+        "<p><strong>$('input, select').serializeAll() output...</strong></p>" +
         "<p class=\"green\">" + serializeAllResults + "</p>"
     );
 }
